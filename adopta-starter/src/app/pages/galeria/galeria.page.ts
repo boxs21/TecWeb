55299b954f6b94189fd2a89db774126f995c080f
Andy Villarroel <a.villarroelg@udd.cl>
2026-09-13 20:50:03 -0300

    feat(galeria): listar todos los perros usando PerrosService
    - Inyecta PerrosService y obtiene todas() en el constructor.
    - Template recorre perros con @for (track perro.id) y renderiza
      una app-tarjeta-perro por cada uno, en grilla responsive
      (ion-grid/ion-row/ion-col size-md="6").
    - Botón "Nuevo" en el toolbar con routerLink a /nuevo.

--- adopta-starter/src/app/pages/galeria/galeria.page.ts
+++ adopta-starter/src/app/pages/galeria/galeria.page.ts
@@ -1,11 +1,20 @@
 import { Component } from '@angular/core';
-import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone';
+import { RouterLink } from '@angular/router';
+import { IonHeader, IonToolbar, IonTitle, IonContent, IonGrid, IonRow, IonCol, IonButton } from '@ionic/angular/standalone';
+import { PerrosService, Perro } from '../../services/perros.service';
+import { TarjetaPerroComponent } from '../../components/tarjeta-perro/tarjeta-perro.component';
 
 @Component({
   selector: 'app-galeria',
   templateUrl: 'galeria.page.html',
   styleUrls: ['galeria.page.scss'],
   standalone: true,
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent],
+  imports: [IonHeader, IonToolbar, IonTitle, IonContent, IonGrid, IonRow, IonCol, TarjetaPerroComponent, IonButton, RouterLink],
 })
-export class GaleriaPage {}
+export class GaleriaPage {
+  perros: Perro[];
+
+  constructor(private perrosService: PerrosService) {
+    this.perros = this.perrosService.todas();
+  }
+}
